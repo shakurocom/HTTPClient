68d9dc682d982fc8175d44087c82662089befbb4
Sergey Laschuk <slaschuk@shakuro.com>
2021-07-12 10:49:28 +0300

    project for framework to test httpclient lib

---
+++ HTTPClient_Framework/HTTPClient_Framework.h
@@ -0,0 +1,15 @@
+//
+//
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HTTPClient_Framework.
+FOUNDATION_EXPORT double HTTPClient_FrameworkVersionNumber;
+
+//! Project version string for HTTPClient_Framework.
+FOUNDATION_EXPORT const unsigned char HTTPClient_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPClient_Framework/PublicHeader.h>
+
+
